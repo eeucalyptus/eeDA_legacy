aea2219776b734ed9b881efe06e88c19c4d08f0b
schwarzwald <schwarzwald@leak-detection.de>
2017-08-30 14:06:28 +0200

    Initial experimental commit

---
+++ __main__.py
@@ -0,0 +1,4 @@
+from app.graphicssurface import GraphicsSurface
+
+if __name__=='__main__':
+    print('''Running eeDA!''')
--- app/graphicssurface.py
+++ app/graphicssurface.py
@@ -1,4 +1,4 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 
 class GraphicsSurface():
-	
+	pass
