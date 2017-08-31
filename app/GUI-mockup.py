38839bc5859225b61d35af7b54d710a3b17d5a39
eeucalyptus <dev@eeucalyptus.net>
2017-08-31 23:30:34 +0200

    Implemented OpenGL triangle example.
    "Triangles, squares man, imagine all these shapes coming together to make a
    guitar."

--- app/GUI-mockup.py
+++ app/GUI-mockup.py
@@ -1,9 +1,7 @@
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+import sys, math
+from PyQt5 import QtCore, QtWidgets, QtGui
 
-class TreeViewWidget(QTreeWidget):
+class TreeViewWidget(QtWidgets.QTreeWidget):
     def __init__(self):
         super().__init__()
         self.initTree()
@@ -11,15 +9,15 @@ class TreeViewWidget(QTreeWidget):
     def initTree(self):
         
         self.setHeaderLabel("Tree")
-        topItem = QTreeWidgetItem()
+        topItem = QtWidgets.QTreeWidgetItem()
         topItem.setText(0, "Root")
-        child = QTreeWidgetItem()
+        child = QtWidgets.QTreeWidgetItem()
         child.setText(0, "Branch")
         topItem.addChild(child)
         self.addTopLevelItem(topItem)
         
 
-class TreeViewDock(QDockWidget):
+class TreeViewDock(QtWidgets.QDockWidget):
     def __init__(self):
         super().__init__()
         self.tree = self.initTV
@@ -31,20 +29,82 @@ class TreeViewDock(QDockWidget):
         self.setWidget(treeWidget)
         treeWidget.setWindowTitle("Tree view")
         return treeWidget
+        
+class GLWidget(QtWidgets.QOpenGLWidget):
+    def __init__(self, parent=None):
+        super(GLWidget, self).__init__(parent)
 
-class MyWindow(QMainWindow):
+
+    def initializeGL(self):
+        version = QtGui.QOpenGLVersionProfile()
+        version.setVersion(2, 1)
+        self.gl = self.context().versionFunctions(version)
+        self.gl.initializeOpenGLFunctions()
+
+        self.gl.glClearColor(0.0, 0.0, 0.0, 1.0)
+        self.object = self.makeTriangle()
+        self.gl.glShadeModel(self.gl.GL_FLAT)
+        self.gl.glEnable(self.gl.GL_DEPTH_TEST)
+        self.gl.glEnable(self.gl.GL_CULL_FACE)
+        self.gl.glEnable(self.gl.GL_MULTISAMPLE)
+
+    def paintGL(self):
+        self.gl.glClear(
+                self.gl.GL_COLOR_BUFFER_BIT | self.gl.GL_DEPTH_BUFFER_BIT)
+        self.gl.glLoadIdentity()
+        self.gl.glTranslated(0.0, 0.0, -10.0)
+        self.gl.glCallList(self.object)
+
+    def resizeGL(self, width, height):
+        side = min(width, height)
+        if side < 0:
+            return
+
+        self.gl.glViewport((width - side) // 2, (height - side) // 2, side, side)
+
+        self.gl.glMatrixMode(self.gl.GL_PROJECTION)
+        self.gl.glLoadIdentity()
+        self.gl.glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0)
+        self.gl.glMatrixMode(self.gl.GL_MODELVIEW)
+
+    def makeTriangle(self):
+    
+        genList = self.gl.glGenLists(1)
+        self.gl.glNewList(genList, self.gl.GL_COMPILE)
+
+        self.gl.glBegin(self.gl.GL_TRIANGLES)
+        
+        self.gl.glColor4f(0.282, 0.235, 0.196, 1.0)
+        
+        for i in range(3):
+            angle = 80.0 - i*120.0
+            x = 0.3 * math.cos(math.radians(angle))
+            y = 0.3 * math.sin(math.radians(angle))
+            self.gl.glVertex3d(x, y, -0.05)
+
+        self.gl.glEnd()
+        self.gl.glEndList()
+
+        return genList
+
+
+class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
         self.initMenu()
         self.initTreeView()
         
-    def initUI(self):
-        self.glWidget = QOpenGLWidget(self)
-        self.txtWidget = QTextEdit(self)
+    def initUI(self):    
+        fmt = QtGui.QSurfaceFormat.defaultFormat()
+        fmt.setSamples(4)
+        QtGui.QSurfaceFormat.setDefaultFormat(fmt)
+        
+        self.glWidget = GLWidget(self)
+        self.txtWidget = QtWidgets.QTextEdit(self)
         self.setCentralWidget(self.glWidget)
         
-        screen = QDesktopWidget().screenGeometry()
+        screen = QtWidgets.QDesktopWidget().screenGeometry()
         self.setGeometry(0, 0, screen.width(), screen.height())
         
         self.setWindowTitle("eeDA 2017 Unprofessional Edition")
@@ -53,17 +113,17 @@ class MyWindow(QMainWindow):
     def initTreeView(self):
         treeview = TreeViewDock()
         treeview.setWindowTitle("Tree View")
-        self.addDockWidget(Qt.LeftDockWidgetArea, treeview)
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, treeview)
         treeview.tree().resize(200, 0)
         
         
     def initMenu(self):
-        exitAct = QAction("Exit", self)
+        exitAct = QtWidgets.QAction("Exit", self)
         exitAct.setShortcut("Ctrl+Q")
         exitAct.setStatusTip("Exit the application.")
         exitAct.triggered.connect(self.close)
         
-        mysteryAct = QAction("Mystery option", self, checkable = True)
+        mysteryAct = QtWidgets.QAction("Mystery option", self, checkable = True)
         mysteryAct.setStatusTip("Not even I know what this does.")
         mysteryAct.setShortcut("Ctrl+Tab")
         mysteryAct.triggered.connect(self.toggleCentralWidget)
@@ -73,8 +133,8 @@ class MyWindow(QMainWindow):
         filemenu.addAction(exitAct)
         filemenu.addAction(mysteryAct)
         
-        moremenu = QMenu("More", self)
-        aboutAct = QAction("About", self)
+        moremenu = QtWidgets.QMenu("More", self)
+        aboutAct = QtWidgets.QAction("About", self)
         aboutAct.setStatusTip("About this software.")
         moremenu.addAction(aboutAct)
         
@@ -84,8 +144,9 @@ class MyWindow(QMainWindow):
         pass
         
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     w = MyWindow()
+    w.setGeometry(300, 300, 300, 300)
     w.showMaximized()
 
     sys.exit(app.exec_())
