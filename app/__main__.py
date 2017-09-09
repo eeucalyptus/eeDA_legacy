e8a12e44fe8a7ff0dbc137d63637985df8e5153e
Musicted <gavin.luedemann@gmail.com>
2017-09-09 12:21:38 +0200

    Added a proper application icon
    I was feeling artsy, so what?

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,14 @@
 import sys
 
 from mainwindow import MyWindow
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 
 if __name__=='__main__':
     print('''Running eeDA!''')
     
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet("QAbstractButton{background: #ffffff} QAbstractButton:hover{background: #cce6ff;border: none}")
+    app.setWindowIcon(QtGui.QIcon('resources\icons\logo.png'))
     w = MyWindow()
     w.setGeometry(300, 300, 300, 300)
     w.showMaximized()
