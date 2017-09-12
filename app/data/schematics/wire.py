c60881c7846df89c608c8a222c42a6daa3afe865
eeucalyptus <dev@eeucalyptus.net>
2017-09-12 06:53:19 +0200

    - Added comments in the beginning of some files (more will follow)

--- app/data/schematics/wire.py
+++ app/data/schematics/wire.py
@@ -1,6 +1,23 @@
+'''
+
+The wire class holds points, forming a line. It consists 
+of two end points (connectors) and multiple (or no) 
+intermediate points. The connectors are either connected 
+to a connection or not at all.
+Wires have to be splitted to attach another connector to
+them, then ending in a junction.
+
+'''
+
+import uuid
+
 from .schematicsconnector import SchematicsConnector
 
 class Wire:
-    def __init__(self):
+    def __init__(self, schematicspage):
+        self.uuid = uuid.uuid1()
+        self.schematicspage = schematicspage
+        
         self.renderer = None
         self.connectors = []
+        self.points = []
