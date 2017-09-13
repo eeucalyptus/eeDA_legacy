04ac409af5fb1e97caf2d54cc453b36a2660a5b1
Musicted <gavin.luedemann@gmail.com>
2017-09-13 17:45:43 +0200

    Adding the connection points to wire rendering
    Partial solution to Issue #1

--- app/graphics/wirerenderer.py
+++ app/graphics/wirerenderer.py
@@ -17,11 +17,35 @@ class WireRenderer(Renderer):
         self.gl.glLineWidth(5)
         
         self.gl.glBegin(self.gl.GL_LINES)
-        
+        if self.wire.connectors[0] != None:
+            point1 = self.wire.connectors[0].pos
+            if len(self.wire.points) > 0:
+                #print("Branch 1")
+                point2 = self.wire.points[0]
+            elif self.wire.connectors[1] != None:
+                #print("Branch 2")
+                point2 = self.wire.connectors[1].pos
+                self.gl.glEnd()
+                self.gl.glEndList()
+                return genList
+            else:
+                #print("Branch 3")
+                self.gl.glEnd()
+                self.gl.glEndList()
+                return genList
+            
+            self.gl.glVertex3i(point1.x, point1.y, 1)
+            self.gl.glVertex3i(point2.x, point2.y, 1)
         
         for i in range(0, len(self.wire.points)-1):
             point1 = self.wire.points[i]
             point2 = self.wire.points[i+1]
+            self.gl.glVertex3i(point1.x, point1.y, 1)
+            self.gl.glVertex3i(point2.x, point2.y, 1)
+        
+        if self.wire.connectors[1] != None:
+            point1 = self.wire.points[-1]
+            point2 = self.wire.connectors[1].pos
             self.gl.glVertex3i(point1.x, point1.y, 1)
             self.gl.glVertex3i(point2.x, point2.y, 1)
         
