04434f56bb4d7c2f59fc68197f3531b64f4defd3
eeucalyptus <dev@eeucalyptus.net>
2017-10-01 21:58:34 +0200

    - Making schematicsloader a class...

--- app/logic/fileloaders/schematicsloader.py
+++ app/logic/fileloaders/schematicsloader.py
@@ -2,130 +2,136 @@ import data.schematics as schematics
 import data.util as util
 import yaml
 
-def loadSchematic(path):
-    filestream = open(path, 'r')
-    filenode = yaml.safe_load(filestream)
+class NoSchematicsFileError(Exception):
+    pass
 
-    # TODO Implement error handling (not found, no schematics, corrupted, etc)
+class SchematicsLoader:
+    def __init__(self, path):
+        self.loadSchematic(path)
+        pass
+    def _loadSchematic(self, path):
+        filestream = open(path, 'r')
+        filenode = yaml.safe_load(filestream)
 
-    schematicnode = filenode.get('schematic', {})
+        # TODO Implement error handling (not found, no schematics, corrupted, etc)
 
-    if schematicnode:
-        return parseSchematic(schematicnode)
-    else:
-        # no schematic
-        print("No schematic")
+        self.schematicnode = filenode.get('schematic', {})
 
+    def _parseSchematic(self):
+        if not self.schematicnode:
+            raise NoSchematicsFileError()
+        schematic = schematics.Schematic()
 
-def parseSchematic(node):
-    schematic = schematics.Schematic()
+        schematic.uuid = node.get('uuid', '')
 
-    schematic.uuid = node.get('uuid', '')
+        for pagenode in node.get('pages', []):
+            page = parsePage(pagenode, schematic)
+            schematic.addPage(page)
 
-    for pagenode in node.get('pages', []):
-        page = parsePage(pagenode, schematic)
-        schematic.addPage(page)
+        schematic.fields = node.get('fields', [])
 
-    schematic.fields = node.get('fields', [])
-
-    return schematic
-
-
-def parsePage(node, schematic):
-    page = schematics.SchematicsPage(schematic)
-
-    page.uuid = node.get('uuid', '')
-    page.schematic = schematic
-
-    for elementnode in node.get('elements', []):
-        element = parseElement(elementnode, page)
-        page.elements.append(element)
-
-    return page
+        return schematic
 
 
-def parseElement(node, page):
-    elementType = node.get('type', 'no_type')
-    switch = {
-        'symbol': parseSymbolElement,
-        'wire': parseWireElement,
-        'decoration': parseDecorationElement,
-        'junction': parseJunctionElement,
-        'label': parseLabelElement
-    }
+    def _parsePage(self, node, schematic):
+        page = schematics.SchematicsPage(schematic)
 
-    parse = switch.get(elementType, parseUnknownElement)
+        page.uuid = node.get('uuid', '')
+        page.schematic = schematic
 
-    return parse(node, page)
+        for elementnode in node.get('elements', []):
+            element = parseElement(elementnode, page)
+            page.elements.append(element)
+
+        return page
 
 
-def parseSymbolElement(node, page):
-    symbol = schematics.Symbol(page, None)
-    symbol.uuid = node.get('uuid', '')
-    symbol.pos.x = node.get('pos', [])[0]
-    symbol.pos.y = node.get('pos', [])[1]
+    def parseElement(self, node, page):
+        elementType = node.get('type', 'no_type')
+        switch = {
+            'symbol': parseSymbolElement,
+            'wire': parseWireElement,
+            'decoration': parseDecorationElement,
+            'junction': parseJunctionElement,
+            'label': parseLabelElement
+        }
 
-    for part in node.get('parts', []):
-        parttype = part.get('type', '')
+        parse = switch.get(elementType, parseUnknownElement)
 
-        if parttype == 'polygon':
-            polygon = parsePolygon(part)
-            symbol.parts.append(polygon)
-
-        elif parttype == 'text':
-            text = parseText(part)
-            symbol.append(text)
-        elif parttype == 'connector':
-            connector = schematics.SymbolConnector(symbol)
-            coonnector.uuid = part.get('uuid', '')
-            connector.parent = symbol
-            connector.pinname = part.get('pinname', '')
-            connector.pinnumber = part.get('pinnumber', '')
-            connector. = part.get('pinname', '')
-            connector.pinname = part.get('pinname', '')
-        else:
-            print("Part type not implemented!")
-
-    print("Symbol!")
-    return symbol
+        return parse(node, page)
 
 
-def parseWireElement(node, page):
-    print("Wire!")
+    def parseSymbolElement(node, page):
+        symbol = schematics.Symbol(page, None)
+        symbol.uuid = node.get('uuid', '')
+        symbol.pos.x = node.get('pos', [])[0]
+        symbol.pos.y = node.get('pos', [])[1]
+
+        for part in node.get('parts', []):
+            parttype = part.get('type', '')
+
+            if parttype == 'polygon':
+                polygon = parsePolygon(part)
+                symbol.parts.append(polygon)
+
+            elif parttype == 'text':
+                text = parseText(part)
+                symbol.append(text)
+            elif parttype == 'connector':
+                connector = schematics.SymbolConnector(symbol)
+                coonnector.uuid = part.get('uuid', '')
+                connector.parent = symbol
+                connector.pinname = part.get('pinname', '')
+                connector.pinnumber = part.get('pinnumber', '')
+                connectorself.otherUUID = ''
+                connector.pinname = part.get('pinname', '')
+            else:
+                print("Part type not implemented!")
+
+        print("Symbol!")
+        return symbol
 
 
-def parseDecorationElement(node, page):
-    print("Decoration!")
+    def parseWireElement(node, page):
+        print("Wire!")
 
 
-def parseJunctionElement(node, page):
-    print("Junction!")
+    def parseDecorationElement(node, page):
+        print("Decoration!")
 
 
-def parseLabelElement(node, page):
-    print("Label!")
+    def parseJunctionElement(node, page):
+        print("Junction!")
 
 
-def parseUnknownElement(node, page):
-    print("Unknown!")
+    def parseLabelElement(node, page):
+        print("Label!")
 
-def parsePolygon(node):
-    ptary = util.PointArray()
 
-    for vertex in part.get('verticies', []):
-        vect = util.Vector2i()
-        vect.x = vertex[0]
-        vect.y = vertex[1]
-        ptary.append(vect)
-    polygon = util.Polygon(ptary)
+    def parseUnknownElement(node, page):
+        print("Unknown!")
 
-    return polygon
+    def parsePolygon(node):
+        ptary = util.PointArray()
 
-def parseText(node):
-    text = schematics.SchematicsText()
-    text.text = part.get('text', '')
-    pos = part.get('pos', [])
-    text.pos = Vector2i(pos[0], pos[1])
-    text.uuid = part.get('uuid', '')
-    text.font = part.get('font', '')
-    text.fontsize = part.get('fontsize', '')
+        for vertex in part.get('verticies', []):
+            vect = util.Vector2i()
+            vect.x = vertex[0]
+            vect.y = vertex[1]
+            ptary.append(vect)
+        polygon = util.Polygon(ptary)
+
+        return polygon
+
+    def parseText(node):
+        text = schematics.SchematicsText()
+        text.text = part.get('text', '')
+        pos = part.get('pos', [])
+        text.pos = Vector2i(pos[0], pos[1])
+        text.uuid = part.get('uuid', '')
+        text.font = part.get('font', '')
+        text.fontsize = part.get('fontsize', '')
+
+if __name__ == '__main__':
+    loader = SchematicsLoader('resources/testschematics.eesc')
+    loader.parseSchematic
